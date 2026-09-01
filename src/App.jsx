ce6f19875ff63c9c7e12a556e294ba08e632f5da
santoshsunuwarmukhiya <santoshsunuwarmukhiya@gmail.com>
2026-09-01 15:09:13 +0545

    added the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import {Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Pricing from './pages/Pricing';
+import Product from './pages/Product';
+
 export default function App() {
   return (
     <>
       <h1>This is a App Component.</h1>
+        <Routes>
+            <Route path="/" component={<HomePage />} />
+            <Route path="/pricing" component={<Pricing />} />
+            <Route path="/product" component={<Product />} />
+        </Routes>
     </>
   )
 }
